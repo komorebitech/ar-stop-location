7a0b4664f68311f9fc720672f3c7da76b7e12d7d
Smit Patel <smit@cityflo.com>
2022-01-31 16:11:40 +0530

    Gltf file for pole

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 window.onload = () => {
-    const params = new URLSearchParams(window.location.search)
-    var latitude =  22.816;
-    var longitude =  70.8385;
-    if(params.has('lat')){
-        latitude = params.get('lat');
+    const params = new URLSearchParams(window.location.search);
+    var latitude = 22.816;
+    var longitude = 70.8385;
+    if (params.has("lat")) {
+        latitude = params.get("lat");
     }
-    if(params.has('long')){
-        longitude = params.get('long')
+    if (params.has("long")) {
+        longitude = params.get("long");
     }
     renderPlaces(latitude, longitude);
 };
 
-
-
 function renderPlaces(latitude, longitude) {
-    let scene = document.querySelector('a-scene');
+    console.log(
+        "🚀 ~ file: script.js ~ line 15 ~ renderPlaces ~ latitude",
+        latitude,
+        longitude
+    );
+    let scene = document.querySelector("a-scene");
 
+    let model = document.createElement("a-entity");
+    model.setAttribute(
+        "gps-entity-place",
+        `latitude: ${latitude}; longitude: ${longitude};`
+    );
+    model.setAttribute("gltf-model", "./assets/pole/scene.gltf");
+    model.setAttribute("rotation", "0 180 0");
+    model.setAttribute("animation-mixer", "");
+    model.setAttribute("scale", "10 10 10");
+    model.setAttribute("position", "0 0.15 -5");
 
-    let model = document.createElement('a-entity');
-    model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-    model.setAttribute('gltf-model', './assets/magnemite/scene.gltf');
-    model.setAttribute('rotation', '0 180 0');
-    model.setAttribute('animation-mixer', '');
-    model.setAttribute('scale', '0.5 0.5 0.5');
-
-    model.addEventListener('loaded', () => {
-        window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+    model.addEventListener("loaded", () => {
+        window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
     });
 
     scene.appendChild(model);
-   
 }
