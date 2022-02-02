8dfbabd751551764ea6341a1a89848e5c100d3b4
Ankit Wala <walaankit921@gmail.com>
2022-02-02 17:39:47 +0530

    removed position

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function renderPlaces(latitude, longitude) {
     model.setAttribute("rotation", "270 180 270");
     model.setAttribute("animation-mixer", "");
     model.setAttribute("scale", "10 10 10");
-    model.setAttribute("position", "0 -2 0");
+    // model.setAttribute("position", "0 -2 0");
 
     model.addEventListener("loaded", () => {
         window.dispatchEvent(new CustomEvent("gps-entity-place-loaded"));
